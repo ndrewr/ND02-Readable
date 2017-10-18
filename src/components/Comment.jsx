a2f8bf13fe9cf25e91ac160df69690e4889767b3
uncleoptimus <uncle.optimus@gmail.com>
2017-10-17 19:33:04 -0700

    Add edit, delete buttons and form toggle to Comment

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,7 +1,7 @@
 // @flow
 
-import React from 'react';
-import { Comment } from 'semantic-ui-react';
+import React, { Component } from 'react';
+import { Button, Comment, Form, Icon } from 'semantic-ui-react';
 
 import ScoreDisplay from './ScoreDisplay';
 
@@ -15,14 +15,29 @@ type CommentFields = {
   author?: string
 };
 
-const CommentItem = ({
-  comment,
-  updateVoteScore
-}: {
-  comment: CommentFields,
+// const CommentItem = ({
+//   comment,
+//   updateVoteScore
+// }: {
+//   comment: CommentFields,
+//   updateVoteScore: (string, string) => void
+// }) => {
+
+type CommentProps = {
+  comment: any,
   updateVoteScore: (string, string) => void
-}) => {
-  const styles = {
+};
+
+type CommentState = {
+  editMode: boolean
+};
+
+class CommentItem extends Component<CommentProps, CommentState> {
+  state = {
+    editMode: false
+  };
+
+  styles = {
     commentContainer: {
       minHeight: '90px'
     },
@@ -31,34 +46,83 @@ const CommentItem = ({
     },
     contentContainer: {
       paddingTop: '20px'
+    },
+    actionButton: {
+      marginLeft: '10px'
     }
   };
 
-  const updateScore = event => {
+  toggleFormVisibility = () => {
+    this.setState(prevState => ({ editMode: !prevState.editMode }));
+  };
+
+  updateScore = event => {
+    const { comment, updateVoteScore } = this.props;
+
     document.activeElement && document.activeElement.blur();
 
     // dispatch vote score update
     updateVoteScore(comment.id, event.currentTarget.value);
   };
 
-  return (
-    <Comment style={styles.commentContainer}>
-      <div style={styles.scoreContainer}>
-        <ScoreDisplay
-          score={comment.voteScore}
-          size="tiny"
-          updateScore={updateScore}
-        />
-      </div>
-      <Comment.Content style={styles.contentContainer}>
-        <Comment.Author as="span">{comment.author} says:</Comment.Author>
-        <Comment.Metadata>
-          <span>on {formatTime(comment.timestamp)}</span>
-        </Comment.Metadata>
-        <Comment.Text>{comment.body}</Comment.Text>
-      </Comment.Content>
-    </Comment>
-  );
-};
+  render() {
+    const { comment } = this.props;
+    const { editMode } = this.state;
+
+    return (
+      <Comment style={this.styles.commentContainer}>
+        <div style={this.styles.scoreContainer}>
+          <ScoreDisplay
+            score={comment.voteScore}
+            size="tiny"
+            updateScore={this.updateScore}
+          />
+        </div>
+        <Comment.Content style={this.styles.contentContainer}>
+          <Comment.Author as="span">{comment.author} says:</Comment.Author>
+          <Comment.Metadata>
+            <span>on {formatTime(comment.timestamp)}</span>
+          </Comment.Metadata>
+          <Comment.Actions as="span">
+            <Comment.Action>
+              <Button
+                compact
+                icon={editMode ? 'left arrow' : 'configure'}
+                size="mini"
+                style={this.styles.actionButton}
+                onClick={this.toggleFormVisibility}
+              />
+            </Comment.Action>
+            <Comment.Action>
+              <Button
+                compact
+                icon="remove"
+                size="mini"
+                style={this.styles.actionButton}
+                onClick={this.toggleFormVisibility}
+              />
+            </Comment.Action>
+          </Comment.Actions>
+          {!editMode ? (
+            <Comment.Text>{comment.body}</Comment.Text>
+          ) : (
+            <Comment.Text>
+              <Form onSubmit={() => console.log('submit')}>
+                <Form.Group>
+                  <Form.Input
+                    placeholder="Change it up"
+                    name="comment_body"
+                    value={comment.body}
+                  />
+                  <Form.Button content="Submit" />
+                </Form.Group>
+              </Form>
+            </Comment.Text>
+          )}
+        </Comment.Content>
+      </Comment>
+    );
+  }
+}
 
 export default CommentItem;
